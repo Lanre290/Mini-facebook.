611836eb742b8e1738c3bbe5b7590ecef90f1ee4
Ashiru Olanrewaju <144792314+Lanre290@users.noreply.github.com>
2024-08-18 21:36:27 +0100

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS','PATCH'],
 
-    'allowed_origins' => ['https://connect-8784.onrender.com'],
+    'allowed_origins' => ['https://connect-8784.onrender.com','*'],
 
     'allowed_origins_patterns' => [],
 
