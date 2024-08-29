0e2cc35ddd8c89aba4562991bfb96cffa0789aac
Ashiru Olanrewaju <lanre2967@gmail.com>
2024-08-29 12:34:24 +0100

    bug fix

--- resources/views/index/notifications.blade.php
+++ resources/views/index/notifications.blade.php
@@ -10,7 +10,7 @@
         @if (null !== session('user'))
             @include('page-sections.left-sidebar', ['id' => session('user')->id, 'active' => 'notifications', 'unread' => $unread])
         @endif
-        <div class="w-full sm:w-full lg:w-9/12 z-10 py-10 flex flex-col min-h-full h-fit overflow-y-auto justify-between">
+        <div class="w-full sm:w-full lg:w-9/12 z-10 py-10 flex flex-col min-h-full h-fit overflow-y-auto">
             @include('includes.notifications',['data' => $data])
         </div>
     </div>
